080b46cd79e76d82b08ceafdf2b3d53df6b8a1fb
Lev <fedorichev16@gmail.com>
2022-12-11 00:36:38 +0300

    fix application label width

--- index.js
+++ index.js
@@ -10,40 +10,40 @@ document.body.style = `background: rgb(59,16,84);
 background: linear-gradient(45deg, rgba(59,16,84,1) 0%, rgba(11,22,61,1) 0%, rgba(11,85,82,1) 100%);`
 
 document.addEventListener("scroll", (e) => {
+    const y = window.pageYOffset;
 
 
-
-    if (window.pageYOffset >= 0 && window.pageYOffset < 400) {
+    if (y >= 0 && y < 400) {
         aboutBlock.classList.add("active");
     } else {
         aboutBlock.classList.remove("active");
     }
 
-    if (window.pageYOffset > 400 && window.pageYOffset < 1300) {
+    if (y > 400 && y < 1300) {
         speakers.classList.add("active");
     } else {
         speakers.classList.remove("active");
     }
 
-    if (window.pageYOffset > 1000 && window.pageYOffset < 1800) {
+    if (y > 1000 && y < 1900) {
         gallaryLabel.classList.add("active");
     } else {
         gallaryLabel.classList.remove("active");
     }
 
-    if (window.pageYOffset > 1800 && window.pageYOffset < 2400) {
+    if (y > 1700 && y < 2400) {
         application.classList.add("active");
     } else {
         application.classList.remove("active");
     }
 
-    if (window.pageYOffset > 2400 && window.pageYOffset < 3000) {
+    if (y > 2400 && y < 3000) {
         mapContacts.classList.add("active");
     } else {
         mapContacts.classList.remove("active");
     }
     document.body.style = `background: rgb(59,16,84);
-    background: linear-gradient(${45 + window.pageYOffset / 2500 * 100}deg, rgba(59,16,84,1) 0%, rgba(11,22,61,1) ${window.pageYOffset / 2500 * 100}%, rgba(11,85,82,1) 100%);`
+    background: linear-gradient(${45 + y / 2500 * 100}deg, rgba(59,16,84,1) 0%, rgba(11,22,61,1) ${y / 2500 * 100}%, rgba(11,85,82,1) 100%);`
 });
 
 
