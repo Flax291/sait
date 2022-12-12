9990b0dabddab40fa6358073e6a4018c8963445f
Федоричев Лев Александрович <19000168@edu.spbgasu.ru>
2022-12-12 18:46:56 +0300

    check mobile version

--- index.js
+++ index.js
@@ -10,43 +10,58 @@ document.body.style = `background: rgb(59,16,84);
 background: linear-gradient(45deg, rgba(59,16,84,1) 0%, rgba(11,22,61,1) 0%, rgba(11,85,82,1) 100%);`
 
 setInterval(() => {
+
     const y = window.pageYOffset;
 
-    if (y >= 0 && y < 400) {
-        aboutBlock.classList.add("active");
+    if (window.screen.width < 700) {
+
     } else {
-        aboutBlock.classList.remove("active");
+
+        if (y >= 0 && y < 400) {
+            aboutBlock.classList.add("active");
+        } else {
+            aboutBlock.classList.remove("active");
+        }
+
+        if (y > 400 && y < 1300) {
+            speakers.classList.add("active");
+        } else {
+            speakers.classList.remove("active");
+        }
+
+        if (y > 1000 && y < 1900) {
+            gallaryLabel.classList.add("active");
+        } else {
+            gallaryLabel.classList.remove("active");
+        }
+
+        if (y > 1700 && y < 2400) {
+            application.classList.add("active");
+        } else {
+            application.classList.remove("active");
+        }
+
+        if (y > 2400 && y < 3000) {
+            mapContacts.classList.add("active");
+        } else {
+            mapContacts.classList.remove("active");
+        }
     }
 
-    if (y > 400 && y < 1300) {
-        speakers.classList.add("active");
-    } else {
-        speakers.classList.remove("active");
-    }
-
-    if (y > 1000 && y < 1900) {
-        gallaryLabel.classList.add("active");
-    } else {
-        gallaryLabel.classList.remove("active");
-    }
-
-    if (y > 1700 && y < 2400) {
-        application.classList.add("active");
-    } else {
-        application.classList.remove("active");
-    }
-
-    if (y > 2400 && y < 3000) {
-        mapContacts.classList.add("active");
-    } else {
-        mapContacts.classList.remove("active");
-    }
     document.body.style = `background: rgb(59,16,84);
     background: linear-gradient(${45 + y / 2500 * 100}deg, rgba(59,16,84,1) 0%, rgba(11,22,61,1) ${y / 2500 * 100}%, rgba(11,85,82,1) 100%);`
 }, 100);
 
 
 aboutBlock.classList.add("active");
+if (window.screen.width < 700) {
+    aboutBlock.classList.add("active");
+    speakers.classList.add("active");
+    gallaryLabel.classList.add("active");
+    application.classList.add("active");
+    mapContacts.classList.add("active");
+
+}
 
 const getCount = () => {
     let xhr = new XMLHttpRequest();
