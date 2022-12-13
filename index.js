31d59ecd5da6d07018b8ec07e837b4820c797e01
Lev <fedorichev16@gmail.com>
2022-12-13 11:09:36 +0300

    fix application form

--- index.js
+++ index.js
@@ -80,25 +80,28 @@ getCount();
 
 const checkName = (name) => {
     const nameRegex = /^[A-Za-zА-Яа-яёЁ]+(?:[-'\s][A-Za-zА-Яа-яёЁ]+)*$/;
-    const prov=nameRegex.test(name);
-    if (name.length<3 ||prov===false){badName(name);} else {goodResponse(name)}
+    const prov = nameRegex.test(name);
+    if (name.length < 3 || prov === false) {
+        return false;
+    } else {
+        return true;
+    }
 
 }
 
 const badName = (name) => {
-
     // TODO: действие при неправильном имени, вывести сообщение под или над полем инпута
     document.getElementById("error-name").textContent = "Некорректное имя";
 }
 
-const checkMail =(email) => {
+const checkMail = (email) => {
     const emailRegex = /^(([0-9A-Za-z]{1}[-0-9A-z\.]{1,}[0-9A-Za-z]{1})|([0-9А-Яа-я]{1}[-0-9А-я\.]{1,}[0-9А-Яа-я]{1}))@([-A-Za-z]{1,}\.){1,2}[-A-Za-z]{2,}$/;
-    const mpr=emailRegex.test(email);
-    if (email.length<6 || mpr===false)
-    {badMail(email);} else {goodResponse(email)
-
+    const mpr = emailRegex.test(email);
+    if (email.length < 6 || mpr === false) {
+        return false;
+    } else {
+        return true;
     }
-
     // TODO: Вернуть true если правильная почта, иначе false
 }
 
@@ -110,13 +113,11 @@ const badMail = (email) => {
 
 const badResponse = () => {
     // TODO: действие при ошибке запроса, например интернета нет
-    const mas =document.getElementById("error-response").textContent = "Что-то произошло не так,но мы уже с этим разбираемся♥";
+    const mas = document.getElementById("error-response").textContent = "Что-то произошло не так,но мы уже с этим разбираемся♥";
 
 }
 const goodResponse = () => {
     // TODO: действие при хорошем ответе Мы вас ждем!Проверяйте вкладку "спам"!
-
-
     document.getElementById("welcome").textContent = "Мы вас ждем!Проверяйте вкладку спам!";
     document.getElementById("error-mail").textContent = "";
     document.getElementById("error-response").textContent = "";
@@ -128,22 +129,30 @@ const sendMail = () => {
     const name = document.getElementById("name").value;
     const mail = document.getElementById("mail").value;
 
-    if (checkName(name)) {
+    document.getElementById("error-mail").textContent = "";
+    document.getElementById("error-response").textContent = "";
+    document.getElementById("error-name").textContent = "";
+    document.getElementById("error-response").textContent = "";
+
+    if (!checkName(name)) {
         badName();
+        return;
     }
-    if (checkMail(mail)) {
+    if (!checkMail(mail)) {
         badMail();
+        return;
     }
 
     let xhr = new XMLHttpRequest();
     xhr.open('GET', `https://vr-days.onrender.com/post_mail?to=${mail}&name=${name}`);
     xhr.send();
     xhr.onload = () => {
-        if (xhr.status !== 200) {
+        if (xhr.status !== 200 || xhr.response == "Fail") {
             badResponse();
         }
-        if (xhr.response == "OK")
+        if (xhr.response == "OK") {
             goodResponse();
+        }
     }
 
 }
